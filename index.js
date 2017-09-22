af97936706eb172c792adcd5bbb673d9aca247c7
Franklyn Thompson <frankistone28@outlook.com>
2017-09-22 12:51:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-var kittens = ["Milo","Otis","Garfield"];
+//var kittens = ["Milo","Otis","Garfield"];
 
 function destructivelyAppendKitten(kittens,kitten){
     return kittens.push(kitten);
