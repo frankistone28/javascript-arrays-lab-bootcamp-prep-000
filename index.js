2800787f5c032707fff520d351afb3838f07a215
Franklyn Thompson <frankistone28@outlook.com>
2017-09-22 13:05:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function destructivelyAppendKitten(name){
 function appendKitten(name){
     return [...kittens, name];
 }
+
+function destructivelyPrependKitten(name){
+    return kittens.unshift(name);
+}
